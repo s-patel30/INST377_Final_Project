5c5d479bf20584b1ca379fa76cc0bc5a1904f417
s-patel30 <65579052+s-patel30@users.noreply.github.com>
2021-06-29 12:28:32 -0400

    sql connection

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+import Sequelize from 'sequelize';
+
 const express = require("express");
+
+var sequelize = new Sequelize('wars_1823_2003', 'student', 'INST377@UMD',{
+    host: '3.236.243.212',
+    dialect: 'mysql'
+});
 
 const app = express();
 
@@ -9,6 +16,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', function(req, res) {
     res.sendFile('index.html')
 });
+
+app.get('/locations', function(req, res){ 
+
+})
 
 app.listen(PORT)
 console.log('Server started at port: ' + PORT);

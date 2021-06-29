85ffcac39c04a2b618fb01dc5d24a51eb076f826
s-patel30 <65579052+s-patel30@users.noreply.github.com>
2021-06-29 12:40:23 -0400

    moved model inside server.js and added get endpoint

--- server.js
+++ server.js
@@ -7,6 +7,24 @@ var sequelize = new Sequelize('wars_1823_2003', 'student', 'INST377@UMD',{
     dialect: 'mysql'
 });
 
+var Locations = sequelize.define(
+    'locations',
+    {
+    location_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+    },
+    location_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    },
+    { timestamps: false }
+);
+
+
+
 const app = express();
 
 const PORT = 3000;
@@ -14,11 +32,12 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
-    res.sendFile('index.html')
+    res.sendFile('index.html');
 });
 
 app.get('/locations', function(req, res){ 
-
+    var locs = Locations.findAll();
+    res.json(locs);
 })
 
 app.listen(PORT)

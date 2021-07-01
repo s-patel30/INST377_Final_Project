7825f5e8e0aa3fa4d2057dac765140fc876e6dad
s-patel30 <65579052+s-patel30@users.noreply.github.com>
2021-06-30 20:55:54 -0400

    connected to PG food + bulma

--- server.js
+++ server.js
@@ -1,35 +1,27 @@
-import Sequelize from 'sequelize';
-
 import Express from 'express';
-
-var sequelize = new Sequelize('wars_1823_2003', 'student', 'INST377@UMD',{
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-var Locations = sequelize.define(
-    'locations',
-    {
-    location_id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-    location_name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-    },
-    { timestamps: false }
-);
-
-
+import Https from 'https';
+import QueryString from 'querystring';
 
 const app = Express();
 
 const PORT = 3000;
 
 app.use(Express.urlencoded({ extended: true }));
+
+const foodAppToken = 'Xs0gbdARsfYYjECTzIQc09FP8';
+
+const foodReqParams = {
+    $limit: 20
+}
+
+const foodReqArgs = QueryString.stringify(foodReqParams);
+
+const foodReqOptions = {
+    hostname: 'data.princegeorgescountymd.gov',
+    port: '443',
+    path: '/resource/umjn-t2iz.json?' + foodReqArgs,
+    method: 'GET'
+}
 
 app.get('/', function(req, res) {
     res.sendFile('table.html', { root: '.' });
@@ -44,15 +36,36 @@ app.get('/modify', function(req, res) {
     res.sendFile('modify.html', { root: '.' });
 });
 
-app.get('/locations', async (req, res) => { 
-    var locs = await Locations.findAll();
-    console.log(locs);
-    res.json(locs);
+app.get('/food', async (req, res) => { 
+    const foodReq = Https.request(foodReqOptions, (foodRes) => {
+        console.log('statusCode:', foodRes.statusCode);
+        var dataBuf = [];
+        foodRes.on('data', function(chunk) {
+            dataBuf.push(chunk);
+        });
+        foodRes.on('end', function() {
+            var data = JSON.parse(Buffer.concat(dataBuf).toString());
+            var resData = [];
+            data.forEach(entry => {
+                var customEntry = {};
+                customEntry['establishment_id'] = entry['establishment_id'];
+                customEntry['name'] = entry['name'];
+                customEntry['address_line_1'] = entry['address_line_1'];
+                customEntry['owner'] = entry['owner'];
+                resData.push(customEntry);
+            })
+            res.json(resData);
+        });
+    });
+    foodReq.on('error', error => {
+        console.error(error);
+    });
+    foodReq.end();
+    console.log(foodReq);
 });
 
-app.put('/locations', async(req, res) => {
-    const newloc = await Locations.create(req.params.location)
-    res.json(newloc.location_id)
+app.put('/food', async(req, res) => {
+    
 });
 
 app.listen(PORT)
